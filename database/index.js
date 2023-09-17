47a35afacca85f6f44dafdca530296e13470e0bd
adrifok <adrifok@gmail.com>
2023-09-16 19:34:20 -0500

    create 3 mongoDBases

---
+++ database/index.js
@@ -0,0 +1,10 @@
+const server = require("./src/server")
+
+const {Character} = 
+
+const PORT = 8004;
+
+server.listen(PORT,()=>{
+    console.log(`Character server listening on port ${PORT}`);
+});
+
---
+++ database/src/database/schemas/planetSchema.js
@@ -0,0 +1,18 @@
+const {Schema} = require("mongoose");
+
+const planetSchema = new Schema({
+
+_id: String,
+name: String,
+rotation_period: String,
+orbital_period: String,
+diameter: String,
+climate: String,
+gravity: String,
+terrain: String,
+surface_water:String,
+res_idents: {type:String, ref:"Planet"},
+films: [{type:String, ref:"Film"}] 
+});
+
+module.exports = planetSchema;
